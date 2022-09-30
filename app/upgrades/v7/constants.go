81c3470b5bc9051a2b3de436e085ce136fe4b067
muhammadp <muhammad@pointlabs.org>
2022-09-30 17:29:27 -0400

    updates constants.go, constants.go, and 6 more files...

--- app/upgrades/v7/constants.go
+++ app/upgrades/v7/constants.go
@@ -8,7 +8,7 @@ const (
 	// TODO: TestnetUpgradeHeight defines the Point testnet block height on which the upgrade will take place
 	TestnetUpgradeHeight = 2_176_500
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Linux_arm64.tar.gz","linux/amd64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Linux_x86_64.tar.gz","windows/x86_64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Windows_x86_64.zip"}}'`
+	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Linux_arm64.tar.gz","linux/amd64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/pointnetwork/point-chain/releases/download/v7.0.0/point_7.0.0_Windows_x86_64.zip"}}'`
 
 	// FaucetAddressFrom is the inaccessible secp address of the Testnet Faucet
 	FaucetAddressFrom = "point1z4ya98ga2xnffn2mhjym7tzlsm49ec23890sze"
